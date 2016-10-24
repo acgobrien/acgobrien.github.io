0e27c934fcaccf91dc34f0a51e7d463f453cff98
Amanda O'Brien <acgobrien@Amanda-OBriens-MacBook-Air.local>
2016-10-24 08:50:47 -0400

    links from thumbnails

--- include/main.js
+++ include/main.js
@@ -2,8 +2,42 @@ $(document).ready(function(){
 
   $("#home-page p").addClass("load");
 
+  // ------------ Back to top button ---------------
 
-  $(".modal, .modal-background").click(function() {
+  jQuery(document).ready(function() {
+
+var offset = 250;
+
+var duration = 300;
+
+jQuery(window).scroll(function() {
+
+if (jQuery(this).scrollTop() > offset) {
+
+jQuery(".back-to-top").fadeIn(duration);
+
+} else {
+
+jQuery(".back-to-top").fadeOut(duration);
+
+}
+
+});
+
+jQuery(".back-to-top").click(function(event) {
+
+event.preventDefault();
+
+jQuery("html, body").animate({scrollTop: 0}, duration);
+
+return false;
+
+})
+
+});
+
+
+  /*$(".modal, .modal-background").click(function() {
 
     // Lock the scroll bar while the modal is active
     $("body").toggleClass("scroll-locked");
@@ -20,7 +54,7 @@ $(document).ready(function(){
       top: $(window).scrollTop() + 70,
       left:left + $(window).scrollLeft()
     });
-  });
+  });*/
 
 });
 
@@ -28,14 +62,14 @@ $(document).ready(function(){
 
 
 	/*if( windowWidth <= 640 ) {
-		if( $('grid-item').length > 0 ) {
-			mobileMenuClone.insertAfter('#menu');
-			$('#navigation-mobile #menu-nav').attr('id', 'menu-nav-mobile');
+		if( $("grid-item").length > 0 ) {
+			mobileMenuClone.insertAfter("#menu");
+			$("#navigation-mobile #menu-nav").attr("id", "menu-nav-mobile");
 		}
 	} else {
-		$('#navigation-mobile').css('display', 'none');
-		if ($('#mobile-nav').hasClass('open')) {
-			$('#mobile-nav').removeClass('open');
+		$("#navigation-mobile").css("display", "none");
+		if ($("#mobile-nav").hasClass("open")) {
+			$("#mobile-nav").removeClass("open");
 		}
 	}
 }*/
